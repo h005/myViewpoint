e4713d53b3dfdb1a7097dea0de4fc60824539905
Wenzhe Zhou <zwzmzd@mail.enjoy-what.com>
2014-09-02 17:05:31 +0800

    修改DLT，没改完

--- DLT/main.cpp
+++ DLT/main.cpp
@@ -471,6 +471,110 @@ bool loadTXT(char* filename, float Cords[10][3], int &count_click)
     return true;
 }
 
+#define NDIM 3
+void normalize_3D(float original[][NDIM], float processed[][NDIM], size_t n, float param[NDIM + 1]) {
+	for (size_t i = 0; i < n; i++) {
+		for (int j = 0; j < NDIM; j++) {
+			processed[i][j] = original[i][j];
+		}
+	}
+
+	// 计算质心，并作移中操作
+	float X[3] = { 0, 0, 0 };
+	for (size_t i = 0; i < n; i++) {
+		for (int j = 0; j < NDIM; j++) {
+			X[j] += processed[i][j];
+		}
+	}
+	for (int j = 0; j < NDIM; j++) {
+		X[j] /= n;
+	}
+	for (size_t i = 0; i < n; i++) {
+		for (int j = 0; j < NDIM; j++) {
+			processed[i][j] -= X[j];
+		}
+	}
+
+	double var = 0;
+	for (size_t i = 0; i < n; i++) {
+		for (int j = 0; j < NDIM; j++) {
+			var += processed[i][j] * processed[i][j];
+		}
+	}
+	var /= n;
+	double s = sqrt(NDIM) / sqrt(var);
+	for (size_t i = 0; i < n; i++) {
+		for (int j = 0; j < NDIM; j++) {
+			processed[i][j] *= s;
+		}
+	}
+
+	// 前面是质心坐标0，最后是缩放比
+	for (int i = 0; i < NDIM; i++) {
+		param[i] = X[i];
+	}
+	param[NDIM] = s;
+}
+#undef NDIM
+
+#define NDIM 2
+void normalize_2D(int original[][NDIM], float processed[][NDIM], size_t n, float param[NDIM + 1]) {
+	for (size_t i = 0; i < n; i++) {
+		for (int j = 0; j < NDIM; j++) {
+			processed[i][j] = original[i][j];
+		}
+	}
+
+	// 计算质心，并作移中操作
+	float X[3] = { 0, 0, 0 };
+	for (size_t i = 0; i < n; i++) {
+		for (int j = 0; j < NDIM; j++) {
+			X[j] += processed[i][j];
+		}
+	}
+	for (int j = 0; j < NDIM; j++) {
+		X[j] /= n;
+	}
+	for (size_t i = 0; i < n; i++) {
+		for (int j = 0; j < NDIM; j++) {
+			processed[i][j] -= X[j];
+		}
+	}
+
+	double var = 0;
+	for (size_t i = 0; i < n; i++) {
+		for (int j = 0; j < NDIM; j++) {
+			var += processed[i][j] * processed[i][j];
+		}
+	}
+	var /= n;
+	double s = sqrt(NDIM) / sqrt(var);
+	for (size_t i = 0; i < n; i++) {
+		for (int j = 0; j < NDIM; j++) {
+			processed[i][j] *= s;
+		}
+	}
+
+	// 前面是质心坐标0，最后是缩放比
+	for (int i = 0; i < NDIM; i++) {
+		param[i] = X[i];
+	}
+	param[NDIM] = s;
+}
+#undef NDIM
+
+void DDLT() {
+	assert((imClick == objClick) && (imClick >= 6)); //必须保证图像与模型对应点数相同且>=6个
+	float *cords3d = new float[imClick * 2];
+	float *cords2d = new float[imClick * 3];
+	float param2d[3], param3d[4];
+
+	normalize_2D(imCords, (float (*)[2])cords2d, imClick, param2d);
+	normalize_3D(objCords,(float (*)[3])cords3d, imClick, param3d);
+
+	float *M = new float[3 * imClick * (9 + imClick)];
+}
+
 void DLT()
 {	
 	assert((imClick == objClick)&&(imClick >= 6)); //必须保证图像与模型对应点数相同且>=6个
