f5b4f598f21e17c1de6aae2af603225eef561e4e
Wenzhe Zhou <zwzmzd@mail.enjoy-what.com>
2014-09-03 23:22:11 +0800

    分解出DLT旋转矩阵，但opengl中的旋转矩阵似乎不一样

--- DLT/main.cpp
+++ DLT/main.cpp
@@ -564,7 +564,7 @@ void normalize_2D(int original[][NDIM], float processed[][NDIM], size_t n, float
 }
 #undef NDIM
 
-void DDLT() {
+void SVDDLT() {
 	// DLT using SVD
 	// Reference: http://www.maths.lth.se/matematiklth/personal/calle/datorseende13/pres/forelas3.pdf
 	assert((imClick == objClick) && (imClick >= 6)); //必须保证图像与模型对应点数相同且>=6个
@@ -572,6 +572,8 @@ void DDLT() {
 	float *cords2d = new float[imClick * 2];
 	float param2d[3], param3d[4];
 
+	// 将选定的点进行正规化，提高SVD分解的数值精度
+	// 正规化使用的参数需要记录下来，以后还原使用
 	normalize_2D(imCords, (float (*)[2])cords2d, imClick, param2d);
 	normalize_3D(objCords,(float (*)[3])cords3d, imClick, param3d);
 
@@ -581,31 +583,26 @@ void DDLT() {
 		printf("%f %f %f %f %f\n", cords3d[i * 3 + 0], cords3d[i * 3 + 1], cords3d[i * 3 + 2], cords2d[i * 2 + 0], cords2d[i * 2 + 1]);
 	}
 
-	float *M = new float[3 * imClick * (12 + imClick)];
-	for (int i = 0; i < 3 * imClick; i++) {
-		for (int j = 0; j < 12 + imClick; j++) {
-			M[i * (12 + imClick) + j] = 0;
-		}
-	}
+	cv::Mat M = cv::Mat::zeros(3 * imClick, 12 + imClick, CV_32F);
 	for (int i = 0; i < imClick; i++) {
 		for (int j = 0; j < 12; j++) {
 			if (j % 4 == 3) {
 				// 3D齐次坐标中的最后一维
-				M[(3 * i + j / 4) * (12 + imClick) + j] = 1;
+				M.at<float>(3 * i + j / 4, j) = 1;
 			} else {
 				// 3D齐次坐标的前三维
-				M[(3 * i + j / 4) * (12 + imClick) + j] = cords3d[i * 3 + j % 4];
+				M.at<float>(3 * i + j / 4, j) = cords3d[i * 3 + j % 4];
 			}
 			
 		}
-		M[(3 * i + 0) * (12 + imClick) + (12 + i)] = -cords2d[i * 2 + 0];
-		M[(3 * i + 1) * (12 + imClick) + (12 + i)] = -cords2d[i * 2 + 1];
-		M[(3 * i + 2) * (12 + imClick) + (12 + i)] = -1;
+		M.at<float>(3 * i + 0 ,12 + i) = -cords2d[i * 2 + 0];
+		M.at<float>(3 * i + 1, 12 + i) = -cords2d[i * 2 + 1];
+		M.at<float>(3 * i + 2, 12 + i) = -1;
 	}
 
 	for (int i = 0; i < 3 * imClick; i++) {
 		for (int j = 0; j < 12 + imClick; j++) {
-			if (abs(M[i * (12 + imClick) + j]) < 0.000001) {
+			if (abs(M.at<float>(i, j)) < 0.000001) {
 				printf("0");
 			} else {
 				printf("x");
@@ -614,37 +611,99 @@ void DDLT() {
 		printf("\n");
 	}
 
-	cv::Mat MM(3 * imClick, 12 + imClick, CV_32F);
-	for (int i = 0; i < MM.rows; i++) {
-		for (int j = 0; j < A.cols; j++) {
-			MM.at<float>(i, j) = M[i * (imClick + 12) + j];
-		}
-	}
+	
 
 	// 执行SVD分解
-	cv::SVD thissvd(MM, cv::SVD::FULL_UV);
+	cv::SVD thissvd(M, cv::SVD::FULL_UV);
 	cv::Mat U = thissvd.u;
 	cv::Mat S = thissvd.w;
 	cv::Mat VT = thissvd.vt;
+
+	// g(v) = 0, 求出使得((Mv)T * (Mv))最小的v
+	// 但是v和-v用于计算二范数是一样的，所以选择λ全部大于零的那个v
 	cv::Mat v = VT.row(S.rows - 1);
+	if (cv::countNonZero(v(cv::Range(0, 1), cv::Range(12, v.cols)) > 0) < (imClick / 2)) {
+		v = -v;
+	}
 
 	std::cout << "SV: " << S.t() << std::endl;
 	std::cout << "v: " << v << std::endl;
-	cv::Mat z = MM * v.t();
+	cv::Mat z = M * v.t();
 	std::cout << sqrt(z.dot(z)) << std::endl;
 
-	
-	cv::Mat A = v(cv::Range(0, 1), cv::Range(0, 12)).reshape(0, 3);
-	std::cout << A << std::endl;
+	// 教程中使用的公式是
+	// λx = PX，但是里面的x和X都是正规化后的，我们需要找到真实的P'
+	// λ(NL * x) = P * (NR * X) => λx = NL(-1) * P * NR * X
+	// 所以 P' = NL(-1) * P * NR
 
-	delete M;
+	// 将用于正规化的矩阵构造出来
+	cv::Mat NR = cv::Mat::eye(4, 4, CV_32F);
+	NR.at<float>(0, 3) = -param3d[0];
+	NR.at<float>(1, 3) = -param3d[1];
+	NR.at<float>(2, 3) = -param3d[2];
+	NR *= param3d[3];
+	std::cout << NR << std::endl;
+
+	cv::Mat NL = cv::Mat::eye(3, 3, CV_32F);
+	NL.at<float>(0, 2) = -param3d[0];
+	NL.at<float>(1, 2) = -param3d[1];
+	NL *= param2d[2];
+	std::cout << NL << std::endl;
+
+	// λ(NL * x) = P * (NR * X)
+	cv::Mat P = v(cv::Range(0, 1), cv::Range(0, 12)).reshape(0, 3);
+	// 求出P'
+	// 因为 λx = NL(-1) * P * NR * X
+	// 所以 P' = NL(-1) * P * NR
+	P = NL.inv() * P * NR;
+
+	// 求解旋转矩阵
+	cv::Mat A = P(cv::Range(0, 3), cv::Range(0, 3));
+	std::cout << A << std::endl;
+	cv::Mat A1 = A.row(0).t();
+	cv::Mat A2 = A.row(1).t();
+	cv::Mat A3 = A.row(2).t();
+	
+	cv::Mat t3 = A3;
+	float f = cv::norm(t3, cv::NORM_L2);
+	cv::Mat R3 = t3 / f;
+
+	float e = A2.dot(R3);
+	cv::Mat t2 = A2 - e * R3;
+	float d = cv::norm(t2, cv::NORM_L2);
+	cv::Mat R2 = t2 / d;
+
+	float c = A1.dot(R3);
+	float b = A1.dot(R2);
+	cv::Mat t1 = A1 - b * R2 - c * R3;
+	float a = cv::norm(t1, cv::NORM_L2);
+	cv::Mat R1 = t1 / a;
+
+	std::cout << R1.dot(R2) << std::endl;
+	std::cout << A3 << f << R3 << std::endl;
+
+	cv::Mat Rotate = cv::Mat::zeros(4, 4, CV_32F);
+	Rotate(cv::Range(0, 1), cv::Range(0, 3)) = R1.t();
+	Rotate(cv::Range(1, 2), cv::Range(0, 3)) = R2.t();
+	Rotate(cv::Range(2, 3), cv::Range(0, 3)) = R3.t();
+	Rotate.at<float>(3, 3) = 1;
+	std::cout << Rotate << std::endl;
+	
+	// 清理空间
 	delete cords2d;
 	delete cords3d;
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			rotation[i * 4 + j] = Rotate.at<float>(i, j);
+		}
+	}
 }
 
 void DLT()
 {	
-	DDLT();
+	SVDDLT();
+	return;
 	assert((imClick == objClick)&&(imClick >= 6)); //必须保证图像与模型对应点数相同且>=6个
 	double a1[12] = {0}, a2[12] = {0};
 	double* A = new double[2*imClick*12];  
@@ -721,6 +780,12 @@ void DLT()
 	SubMatrix(L+8,L+8,rota+8,0,1/x,1,3);
 
 	TranMatrix(rota,rotation,4,4);
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			printf("%f ", rotation[i * 4 + j]);
+		}
+		printf("\n");
+	}
 }
 
 void
