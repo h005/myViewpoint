8ae06d52786bb9a645143bff1d7d03bbf11e13fa
Wenzhe Zhou <zwzmzd@mail.enjoy-what.com>
2014-10-22 13:37:02 +0800

    显示对极线

--- vl-client/vl-client/main.cpp
+++ vl-client/vl-client/main.cpp
@@ -12,30 +12,11 @@ extern "C" {
 
 #include "def.h"
 #include "FeatureExtraction.h"
+#include "calibration.h"
+#include "utils.h"
 
 using namespace cv;
 using namespace std;
-
-typedef struct _color{
-	float red;
-	float green;
-	float blue;
-} color;
-
-color pointColor[12] = {
-	{ 1.0, 0.0, 0.0 },
-	{ 0.0, 1.0, 0.0 },
-	{ 0.0, 0.0, 1.0 },
-	{ 1.0, 1.0, 0.0 },
-	{ 1.0, 0.0, 1.0 },
-	{ 0.0, 1.0, 1.0 },
-	{ 0.5, 0.0, 0.0 },
-	{ 1.0, 0.6, 0.0 },
-	{ 0.8, 0.6, 1.0 },
-	{ 0.0, 0.6, 0.2 },
-	{ 0.0, 0.6, 1.0 },
-	{ 0.6, 0.0, 1.0 },
-};
 
 static inline int convertRadianToDegree(double rad) {
 	int deg = (int)(rad / M_PI * 180);
@@ -69,16 +50,6 @@ static void convertSingleFeatureToMat(const vector<singleFeature *> &feats, int 
 		}
 	}
 	out = query;
-}
-
-// 将两幅图像横向拼接在一起
-static void combineImage(const Mat &qImg, const Mat &tImg, Mat &output) {
-	Mat panel(std::max<int>(qImg.rows, tImg.rows), qImg.cols + tImg.cols, qImg.type());
-	panel.setTo(0);
-	qImg.copyTo(panel(Rect(0, 0, qImg.cols, qImg.rows)));
-	tImg.copyTo(panel(Rect(qImg.cols, 0, tImg.cols, tImg.rows)));
-
-	output = panel;
 }
 
 // 使用给定的特征，对图像进行匹配
@@ -187,95 +158,12 @@ static void constructMatchPairs(const char *queryImagePath, const char *trainIma
 	assert(qMatchPoints.size() == tMatchPoints.size());
 }
 
-// 将匹配点在图像中显示出来
-static void explore_match(const Mat &qImg, const Mat &tImg, const vector<KeyPoint> &qMatchPoints, const vector<KeyPoint> &tMatchPoints, char *fileName) {
-	if (fileName) {
-		assert(qMatchPoints.size() == tMatchPoints.size());
-
-		Mat panel;
-		combineImage(qImg, tImg, panel);
-
-		int n = sizeof(pointColor) / sizeof(color);
-
-		for (size_t i = 0; i < qMatchPoints.size(); i++) {
-			Point2i p1 = qMatchPoints[i].pt, p2 = tMatchPoints[i].pt;
-			p2.x += qImg.cols;
-
-			Scalar color(pointColor[i % n].blue * 255, pointColor[i % n].green * 255, pointColor[i % n].red * 255);
-			circle(panel, p1, 2, color, -1);
-			circle(panel, p2, 2, color, -1);
-
-			line(panel, p1, p2, color, 1, CV_AA);
-		}
-		imwrite(fileName, panel);
-	}
-}
-
-static bool loadTXT(char* filename, vector<Point2f> &plist, int height)
-{
-	FILE* fp;
-	fp = fopen(filename, "rb");
-	if (!fp) {
-		perror(filename);
-		return false;
-	}
-
-	// feof很不好用，很容易出现最后一行读两次的情况
-	// http://stackoverflow.com/questions/5431941/while-feof-file-is-always-wrong
-	// 这样处理之后，*_im_norm.txt文件的最后一行可以为空
-	Point2f p;
-	int x, y;
-	while (fscanf(fp, "%d %d", &x, &y) != EOF) {
-		// 文件中的点坐标系在左下角，需要转化为opencv中的点坐标
-		p.x = x;
-		p.y = height - y;
-		plist.push_back(p);
-	}
-	fclose(fp);
-	return true;
-}
-
-// 将变换后的点在train图像中显示出来
-static void explore_point_homograhy(const Mat &qImg, const Mat &tImg, char *qPointsFile, const Mat &H, char *fileName) {
-	// 读取query图像中标定的点
-	vector<Point2f> plist;
-	loadTXT(qPointsFile, plist, qImg.size().height);
-
-	// 将点在query图像中表示出来
-	Mat left;
-	qImg.copyTo(left);
-	for (int i = 0; i < plist.size(); i++) {
-		Scalar pColor(pointColor[i].blue * 255, pointColor[i].green * 255, pointColor[i].red * 255);
-		circle(left, plist[i], 4, pColor, -1);
-	}
-
-	// 使用homography变换plist中的点
-	vector<Point2f> mlist;
-	perspectiveTransform(plist, mlist, H);
-
-	// 将变换后的点在train图像中表示出来
-	// 如果点在train图像中也能精确对应建筑物上的相应点，则表示homography效果好
-	// 不过一般不存在这种情况，至多一个平面上的点精确对应
-	Mat right;
-	tImg.copyTo(right);
-	for (int i = 0; i < mlist.size(); i++) {
-		Scalar pColor(pointColor[i].blue * 255, pointColor[i].green * 255, pointColor[i].red * 255);
-		circle(right, mlist[i], 4, pColor, -1);
-	}
-
-	// 将左右的图像画在一起
-	Mat panel;
-	combineImage(left, right, panel);
-	imwrite(fileName, panel);
-}
-
 int main()
 {
 	
-	/*char *queryImg = "2q.png", *trainImg = "2t.png";
-	char *im_norm = "2_im_norm.txt";*/
+	/*char *queryImg = "2q.png", *trainImg = "2t.png";*/
 	char *queryImg = "1q.ppm", *trainImg = "1t.png";
-	char *im_norm = "1_im_norm.txt";
+	/*char *queryImg = "et000.jpg", *trainImg = "et001.jpg";*/
 	Mat qImg = imread(queryImg), tImg = imread(trainImg);
 
 	vector<KeyPoint> qKeyPoints, tKeyPoints;
@@ -331,36 +219,20 @@ int main()
 	fclose(pFile);
 
 	Mat mask;
-	Mat F = findFundamentalMat(qPoints, tPoints, CV_FM_LMEDS, 1, 0.99, mask);
-	cout << F << endl;
-	//Mat H = findHomography(qPoints, tPoints, CV_RANSAC, 3, mask);
-
+	Mat F = findFundamentalMat(qPoints, tPoints, CV_FM_RANSAC, 1, 0.99, mask);
+	
+	vector<Point> qFilteredPoints, tFilteredPoints;
 	vector<KeyPoint> qFilteredKeyPoints, tFilteredKeyPoints;
 	for (int i = 0; i < mask.rows; i++) {
 		if (mask.at<uchar>(i, 0)) {
+			qFilteredPoints.push_back(qPoints[i]);
+			tFilteredPoints.push_back(tPoints[i]);
 			qFilteredKeyPoints.push_back(qKeyPoints[i]);
 			tFilteredKeyPoints.push_back(tKeyPoints[i]);
 		}
 	}
 	explore_match(qImg, tImg, qFilteredKeyPoints, tFilteredKeyPoints, "Ransaced.png");
-	
-	/*cout << H << endl;
-	Mat output(qImg.size(), qImg.type());
-	warpPerspective(qImg, output, H, output.size());
-	imshow("warpPerspective", output);
-	imwrite("warpPerspective.png", output);
-
-	explore_point_homograhy(qImg, tImg, im_norm, H, "twp.png"); 
-
-	Mat ma = Mat::eye(3, 3, CV_64F);
-	ma.at<double>(1, 1) = -1;
-	ma.at<double>(1, 2) = qImg.size().height;
-
-	Mat mb = Mat::eye(3, 3, CV_64F);
-	mb.at<double>(1, 1) = -1;
-	mb.at<double>(1, 2) = tImg.size().height;
-
-	cout << mb * H * ma.inv() << endl;*/
+	drawEpipolarLines(F, qImg, tImg, qPoints, tPoints, -1, "epipolar.png");
 
 	waitKey();
 	destroyAllWindows();
