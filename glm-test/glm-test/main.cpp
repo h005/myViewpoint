55464cbcb3903aa92486160439929f9d8a42e4fb
Wenzhe Zhou <zwzmzd@mail.enjoy-what.com>
2014-12-01 02:17:28 +0800

    增加glm的测试程序

---
+++ glm-test/glm-test/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <glm/glm.hpp> 
+#include <glm/gtc/constants.hpp> 
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+int main() {
+	glm::mat4 rotate = glm::rotate(glm::mat4(1.f), glm::pi<float>() / 2, glm::vec3(0.f, 0.f, 1.f));
+	glm::vec4 point = glm::vec4(glm::vec3(1.f, 0.f, 0.f), 1.f);
+	std::cout << glm::to_string(rotate * point) << std::endl;
+	std::cout << glm::to_string(rotate) << std::endl;
+
+	return 0;
+}
