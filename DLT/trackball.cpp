5d81eb8fcc57981e0a26211854c70d7f3b42f8bf
Wenzhe Zhou <zwzmzd@mail.enjoy-what.com>
2014-12-07 16:34:47 +0800

    更换轨迹球算法

--- DLT/trackball.cpp
+++ DLT/trackball.cpp
@@ -1,31 +1,32 @@
 #include "trackball.h"
 #include <iostream>
 
-#define R 2.f
+#define R 0.5f
 
 static void computeNormVec3(const glm::vec2 &in, glm::vec3 &out) {
-	float z;
-	if (in.x * in.x + in.y * in.y <= R * R / 2) {
-		z = sqrt(R * R - in.x * in.x - in.y * in.y);
+	float d2 = (in.x * in.x + in.y * in.y);
+	if (d2 <= R * R - 0.0001) {
+		out.x = in.x;
+		out.y = in.y;
+		out.z = sqrt(R * R - d2);
 	}
 	else {
-		z = R * R / 2.f / sqrt(in.x * in.x + in.y * in.y);
+		out.x = in.x;
+		out.y = in.y;
+		out.z = 0;
 	}
-	out.x = in.x;
-	out.y = in.y;
-	out.z = z;
+	
 	out = glm::normalize(out);
 }
 
 void computeRotation(const glm::vec2 &start, const glm::vec2 &end, glm::vec3 &N, float &angle) {
 	// Reference: http://www.lubanren.net/weblog/post/283.html
-	glm::vec3 a, b;
+	glm::vec3 a;
 	computeNormVec3(start, a);
-	computeNormVec3(end, b);
 
-	N = glm::cross(a, b);
+	glm::vec3 t(end - start, 0.f);
 
-	float x = glm::angle(a, b) / glm::pi<float>();
-	x = (-x * x + 2 * x);
-	angle = x * glm::pi<float>();
+	N = glm::cross(a, t);
+	angle = glm::length(t) / R;
+	printf("%f %f %f angle %f\n", N.x, N.y, N.z, angle);
 }
