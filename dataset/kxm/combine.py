15313f552ab769552da9357c5a0f14c034e6a5d5
Wenzhe Zhou <zwzmzd@mail.enjoy-what.com>
2015-11-06 14:20:05 +0800

    允许多个特征文件

--- dataset/kxm/combine.py
+++ dataset/kxm/combine.py
@@ -3,69 +3,48 @@ import sys
 import optparse
 
 if __name__ == '__main__':
-    usage = 'Usage: %prog --2df=kxm.2df --3df=kxm.3df --label=kxm.label --output=kxm --libsvm'
+    usage = 'Usage: %prog --label=kxm.label --output=kxm --libsvm kxm.2df kxm.3df [kxm2.3df]'
     parser = optparse.OptionParser(usage=usage)
-    parser.add_option('--2df', dest='f2d', help='extracted 2d features')
-    parser.add_option('--3df', dest='f3d', help='extracted 3d features')
     parser.add_option('--label', dest='label', help='labels for training')
     parser.add_option('--output', dest='output', help='output data file name')
     parser.add_option('--libsvm', dest='output_libsvm', action='store_true', help='output file with libsvm format')
-    options, args = parser.parse_args()
+    options, feature_files = parser.parse_args()
 
     data = {}
 
     length = -1
-    f2d = open(options.f2d, 'r')
-    while True:
-        key = f2d.readline().strip()
-        features = f2d.readline().strip()
-        if not key:
-            break
-        data[key] = features
-        
-        # 检查向量的长度是否一致
-        current_length = len(data[key].split(' '))
-        if length < 0:
-            length = current_length
-        else:
-            assert(length == current_length)
-    f2d.close()
+    with open(options.label, 'r') as label:
+        while True:
+            key = label.readline().strip()
+            value = label.readline().strip()
+            if not key:
+                break
 
-    length = -1
-    f3d = open(options.f3d, 'r')
-    while True:
-        key = f3d.readline().strip()
-        features = f3d.readline().strip()
-        if not key:
-            break
-        assert(key in data)
-        data[key] = data[key] + " " + features
+            data[key] = value 
+            # 检查向量的长度是否一致
+            current_length = len(data[key].split(' '))
+            if length < 0:
+                length = current_length
+            else:
+                assert(length == current_length)
 
-        # 检查向量的长度是否一致
-        current_length = len(data[key].split(' '))
-        if length < 0:
-            length = current_length
-        else:
-            assert(length == current_length)
-    f3d.close()
-
-    length = -1
-    label = open(options.label, 'r')
-    while True:
-        key = label.readline().strip()
-        value = label.readline().strip()
-        if not key:
-            break
-        assert(key in data)
-        data[key] = value + " " + data[key] 
-
-        # 检查向量的长度是否一致
-        current_length = len(data[key].split(' '))
-        if length < 0:
-            length = current_length
-        else:
-            assert(length == current_length)
-    label.close()
+    for feature_file in feature_files:
+        length = -1
+        with open(feature_file, 'r') as ff:
+            while True:
+                key = ff.readline().strip()
+                features = ff.readline().strip()
+                if not key:
+                    break
+                assert(key in data)
+                data[key] = data[key] + ' ' + features
+                
+                # 检查向量的长度是否一致
+                current_length = len(data[key].split(' '))
+                if length < 0:
+                    length = current_length
+                else:
+                    assert(length == current_length)
 
     if options.output_libsvm:
         output_data = open('%s.data' % options.output, 'w')
