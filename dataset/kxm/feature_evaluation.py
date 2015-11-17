232a0e648bd930ca81ca4b7ecf12f924a1e15184
Wenzhe Zhou <zwzmzd@mail.enjoy-what.com>
2015-11-17 18:41:18 +0800

    脚本调整

--- dataset/kxm/feature_evaluation.py
+++ dataset/kxm/feature_evaluation.py
@@ -1,13 +1,33 @@
 #-*-coding:utf-8-*-
-import combine
+import subprocess
+import re
+import sys
+import pprint
 
+IN = 'kxm.data'
 if __name__ == '__main__':
     # 搜集所有的df文件
 
-    dfs = ['1.df', '2.df', '3.df']
-    for df in dfs:
-        input_file, = generate_file('kxm.label', [df], True)
-        with open('../../kxm.data', 'w') as f:
-            f.write(input_file)
+    with open(IN, 'r') as indata:
+        v = indata.readline().strip()
+        feature_count = len(v.split(' ')) - 1
 
+    result = {}
+    for df in xrange(feature_count):
+        with open('kxm.data', 'r') as indata:
+            with open(r'C:\Users\mzd\Desktop\viewpoint\libsvm-3.20-auc-mod\kxm.data', 'w') as f:
+                for line in indata:
+                    elements = line.strip().split(' ')
+                    f.write('%s %s\n' % (elements[0], elements[df+1]))
+        
         # 运行easy.py
+        command = 'python easy.py ../kxm.data'
+        process = subprocess.Popen(command, cwd=r'C:\Users\mzd\Desktop\viewpoint\libsvm-3.20-auc-mod\tools', stdout=subprocess.PIPE)
+        output = process.communicate()[0]
+        sys.stderr.write('%d\n' % df)
+        sys.stderr.write(output)
+        ex_name_re=re.search(r'CV rate=[\d]+.[\d]+', output)
+        val = float(ex_name_re.group(0)[len('CV rate='):].strip())
+        result[df] = val
+
+    pprint.pprint(list(sorted(result.items(), key=lambda d:d[1], reverse=True)))
