6df9fcf17cb01e8420e385ad78b058d5376244d5
Wenzhe Zhou <zwzmzd@mail.enjoy-what.com>
2015-12-20 21:45:44 +0800

    第一步： 从list文件中生成配置
    第二步： 批量提交本目录下的配置文件

---
+++ amtsurvey/batch_post.py
@@ -0,0 +1,67 @@
+#-*- coding:utf-8 -*-
+import cv2
+import numpy
+import optparse
+import os
+import sys
+import shutil
+import pprint
+import time
+import subprocess
+import filecmp
+import re
+import time
+import os
+
+AMT_DIR = r'D:\aws-mturk-clt-1.3.1'
+JAVA_HOME = r'C:\Program Files\Java\jdk1.8.0_25'
+def prepare_env():
+    os.environ['JAVA_HOME'] = JAVA_HOME
+    os.environ['PATH'] = os.path.join(JAVA_HOME, 'bin') + ';' + os.environ['PATH']
+    classpath = []
+    classpath.append(os.path.join(AMT_DIR, 'bin'))
+    classpath.append(os.path.join(JAVA_HOME, 'lib'))
+    classpath.append(os.path.join(JAVA_HOME, 'lib', 'tools.jar'))
+    os.environ['CLASSPATH'] = ';'.join(classpath)
+    os.environ['MTURK_CMD_HOME'] = AMT_DIR
+
+
+def walker(dir_path):
+    if len(dir_path) == 0:
+        dir_path = '.'
+    
+    count = 0
+    for root, dirs, files in os.walk(dir_path):
+        for name in files:
+            fname, ext = os.path.splitext(name)
+            if ext.lower() in ('.properties',):
+                abs_path = os.path.abspath(root)
+                pf = os.path.join(abs_path, fname + '.properties')
+                qf = os.path.join(abs_path, fname + '.question')
+                inf = os.path.join(abs_path, fname + '.input')
+
+                print pf, qf, inf
+
+                command = 'loadHITs -question %s -properties %s -input %s -sandbox' % (qf, pf, inf)
+                wdir = os.path.join(AMT_DIR, 'bin')
+
+                import subprocess
+                p = subprocess.Popen(command, cwd=wdir, stdout=subprocess.PIPE, shell=True)
+                print p.communicate()[0]
+
+                count += 1
+                if count == 2:
+                    break
+                
+
+if __name__ == '__main__':
+    # For more details about optparser, Please visit:
+    # https://docs.python.org/2/library/optparse.html#default-values
+
+    usage = 'Usage: %prog --dir=. [--force]' 
+    parser = optparse.OptionParser(usage = usage)
+    parser.add_option('-d', '--dir', dest='dir', default = '.', help="source file's dir")
+    (options, args) = parser.parse_args()
+
+    prepare_env()
+    walker(os.path.dirname(options.dir))
